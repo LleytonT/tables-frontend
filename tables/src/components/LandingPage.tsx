6acbd9968578206ee742d7faacada1857a267d41
Lleyton PC <z5316214@ad.unsw.edu.au>
2024-07-25 23:30:57 +1000

    Added mobile responsiveness

--- tables/src/components/LandingPage.tsx
+++ tables/src/components/LandingPage.tsx
@@ -8,7 +8,6 @@ import {
   SignUpButton,
   SignOutButton,
 } from "@clerk/clerk-react";
-import Typewriter from 'typewriter-effect';
 import { Box, Card, CardMedia, Typography, CardContent } from "@mui/material";
 
 const NavBar: React.FC = () => {
@@ -58,7 +57,7 @@ const NavBar: React.FC = () => {
       </div>
     </nav>
   );
-}
+};
 // Project type
 type Project = {
   title: string;
@@ -97,7 +96,7 @@ const projects: Project[] = [
     description: "sample app",
     image:
       "https://cdn.dribbble.com/userupload/11743954/file/original-9095ba50c038e3a143ebbdcf41cc6ead.png?resize=2048x1536",
-  }
+  },
   // Add more projects here
 ];
 
@@ -107,35 +106,41 @@ const ProjectComponent: React.FC<{ project: Project }> = ({ project }) => {
   };
 
   return (
-      <Card style={{ width: 300, height: 300 }}>
-        <div style={{ position: 'relative', width: '100%', height: '100%' }}>
-          <div style={{ width: '100%', height: '100%' }}>
+    <Card style={{ width: 300, height: 300 }}>
+      <div style={{ position: "relative", width: "100%", height: "100%" }}>
+        <div style={{ width: "100%", height: "100%" }}>
           <CardMedia
-            sx={{ height: '100%' }}
+            sx={{ height: "100%" }}
             image={project.image}
             title={project.title}
           />
-          </div>
-          <CardContent style={{
-            position: 'absolute',
+        </div>
+        <CardContent
+          style={{
+            position: "absolute",
             bottom: 0,
             left: 0,
-            width: '100%',
-            backgroundColor: 'black',
-            color: 'white',
-          }}>
-            <Typography gutterBottom variant="h5" component="div" style={{ color: 'white' }}>
-              {project.title}
-            </Typography>
-            <Typography variant="body2" style={{ color: 'white' }}>
-              {project.description}
-            </Typography>
-          </CardContent>
-        </div>
-      </Card>
-    );
+            width: "100%",
+            backgroundColor: "black",
+            color: "white",
+          }}
+        >
+          <Typography
+            gutterBottom
+            variant="h5"
+            component="div"
+            style={{ color: "white" }}
+          >
+            {project.title}
+          </Typography>
+          <Typography variant="body2" style={{ color: "white" }}>
+            {project.description}
+          </Typography>
+        </CardContent>
+      </div>
+    </Card>
+  );
 };
-
 
 const LandingPage: React.FC = () => {
   const [email, setEmail] = useState<string>("");
@@ -171,43 +176,74 @@ const LandingPage: React.FC = () => {
     "Who can help me improve the storytelling aspect of my pitch?",
   ];
 
-
   return (
     <>
-    <NavBar />
-    <div className="flex flex-col justify-center items-center h-screen" >
-      <h1 className="text-6xl font-bold mb-4">The Community For Creators</h1>
-      <div style={{width: "50%"}}>
-      <Box component="section" sx={{ p: 2, border: '2px dashed blue', borderRadius: 8}}>
-          <Typewriter
-          options={{loop: true}}
-       onInit={(typewriter) => {
-        searchTerm.forEach((search) => {
-          typewriter.changeDelay(50).typeString(search).pauseFor(2000).changeDeleteSpeed(25).deleteAll().start();
-        });
-       }}
-      />
-      </Box>
-        <p className="text-lg mt-4 mb-8" style={{ textAlign: 'center' }}>
-        Say goodbye to endless searching and hello to finding the perfect match. Tables has an AI-powered search engine to connect you to the community and projects that matter to you.
-        </p>
-      </div>
-    
-      <p className="text-lg mt-4">Drop your email.</p>
-      <p className="text-lg mt-2">We'll let you know when we're ready!</p>
-      <form action="https://send.pageclip.co/FzLh5buxmmEsLZ1slQfz7xaoiNd8MW0a/waitlist" className="pageclip-form" method="post">
-        <div className="flex flex-col justify-center" >
-            <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} className="w-80 h-10 border-solid border-2 border-black rounded shadow-sm text-black bg-white mt-2" placeholder=" Name"/>
-            <input type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} className="w-80 h-10 border-solid border-2 border-black rounded shadow-sm text-black bg-white mt-2" placeholder=" Email"/>
-            <button type="submit" className="w-80 h-10 bg-black text-white rounded mt-2 pageclip-form__submit">
-              <span>Submit</span>
-            </button>
+      <div className="flex flex-col h-screen">
+        <NavBar />
+        <div className="flex flex-col justify-center items-center h-screen">
+          <h1 className="text-4xl md:text-6xl font-bold mb-4 text-center">
+            The Community For Creators
+          </h1>
+          <div style={{ width: "50%" }}>
+            <Box
+              component="section"
+              sx={{ p: 2, border: "2px dashed blue", borderRadius: 8 }}
+            >
+              <Typewriter
+                options={{ loop: true }}
+                onInit={(typewriter) => {
+                  searchTerm.forEach((search) => {
+                    typewriter
+                      .changeDelay(50)
+                      .typeString(search)
+                      .pauseFor(2000)
+                      .changeDeleteSpeed(25)
+                      .deleteAll()
+                      .start();
+                  });
+                }}
+              />
+            </Box>
+            <p className="text-base md:text-lg mt-4 mb-8 text-center">
+              Say goodbye to endless searching and hello to finding the perfect
+              match. Tables has an AI-powered search engine to connect you to
+              the community and projects that matter to you.
+            </p>
+          </div>
+          <p className="text-lg mt-4">Drop your email.</p>
+          <p className="text-lg mt-2">We'll let you know when we're ready!</p>
+          <form
+            action="https://send.pageclip.co/FzLh5buxmmEsLZ1slQfz7xaoiNd8MW0a/waitlist"
+            className="pageclip-form"
+            method="post"
+          >
+            <div className="flex flex-col justify-center">
+              <input
+                type="text"
+                name="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="w-80 h-10 border-solid border-2 border-black rounded shadow-sm text-black bg-white mt-2"
+                placeholder=" Name"
+              />
+              <input
+                type="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="w-80 h-10 border-solid border-2 border-black rounded shadow-sm text-black bg-white mt-2"
+                placeholder=" Email"
+              />
+              <button
+                type="submit"
+                className="w-80 h-10 bg-black text-white rounded mt-2 pageclip-form__submit"
+              >
+                <span>Submit</span>
+              </button>
+            </div>
+          </form>
         </div>
-      </form>
-
-      
-
-    </div>
+      </div>
     </>
   );
 };
