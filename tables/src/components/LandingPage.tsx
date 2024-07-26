d86ca205a8a7c241e75b70ef6c70b2ff669f5373
Aditya <FLAG{AdI7yA_w45_h3r3}>
2024-07-26 19:11:39 +1000

    Rearranged submission box in page + fixed issue submitting empty fields

--- tables/src/components/LandingPage.tsx
+++ tables/src/components/LandingPage.tsx
@@ -19,7 +19,6 @@ const NavBar: React.FC = () => {
 const LandingPage: React.FC = () => {
   const [email, setEmail] = useState<string>("");
   const [name, setName] = useState<string>("");
-  const [showNotification, setShowNotification] = useState(false);
 
   useEffect(() => {
     const script = document.createElement('script');
@@ -32,14 +31,6 @@ const LandingPage: React.FC = () => {
       document.body.removeChild(script);
     };
   }, [])
-
-  const handleSubmit = (e: React.FormEvent) => {
-    if (!name || !email) {
-      e.preventDefault();
-      setShowNotification(true);
-      setTimeout(() => setShowNotification(false), 3000); // Hide notification after 3 seconds
-    }
-  };
 
   const searchTerm = [
     "Anyone here making flying cars",
@@ -96,11 +87,30 @@ const LandingPage: React.FC = () => {
           </p>
         </div>
 
+        <h2 className="text-4xl text-center font-bold text-gray-900 py-4 mt-24">
+          Join Our Waitlist
+        </h2>
+
+        <p className="text-lg mt-4">Drop your email. We'll let you know when we're ready!</p>
+
+        <form
+          action="https://send.pageclip.co/FzLh5buxmmEsLZ1slQfz7xaoiNd8MW0a/waitlist"
+          className="pageclip-form"
+          method="post">
+          <div className="flex flex-col justify-center" >
+            <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} className="w-80 h-10 border-solid border-2 border-black rounded shadow-sm text-black bg-white mt-2" placeholder=" Name" required />
+            <input type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} className="w-80 h-10 border-solid border-2 border-black rounded shadow-sm text-black bg-white mt-2" placeholder=" Email" required />
+            <button type="submit" className="w-80 h-10 bg-black text-white rounded mt-2 pageclip-form__submit">
+              <span>Submit</span>
+            </button>
+          </div>
+        </form>
+
         <section className="py-24">
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div className="mb-14 text-center">
               <h2 className="text-4xl text-center font-bold text-gray-900 py-5">
-                Find and Be Found
+                Features
               </h2>
               <p className="text-lg font-normal text-gray-500 max-w-md md:max-w-2xl mx-auto">
                 You focus on what you do best, we'll handle the rest.
@@ -182,44 +192,9 @@ const LandingPage: React.FC = () => {
           </div>
         </section>
 
-
-        <p className="text-lg mt-4">Drop your email.</p>
-        <p className="text-lg mt-2">We'll let you know when we're ready!</p>
-
-        <form
-          action="https://send.pageclip.co/FzLh5buxmmEsLZ1slQfz7xaoiNd8MW0a/waitlist"
-          className="pageclip-form"
-          method="post"
-          onSubmit={handleSubmit}>
-          <div className="flex flex-col justify-center" >
-            <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} className="w-80 h-10 border-solid border-2 border-black rounded shadow-sm text-black bg-white mt-2" placeholder=" Name" />
-            <input type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} className="w-80 h-10 border-solid border-2 border-black rounded shadow-sm text-black bg-white mt-2" placeholder=" Email" />
-            <button type="submit" className="w-80 h-10 bg-black text-white rounded mt-2 pageclip-form__submit">
-              <span>Submit</span>
-            </button>
-          </div>
-        </form>
-
         <div className="mb-16"></div>
 
       </div>
-      {showNotification && (
-        <div id="toast-default" className="fixed top-4 right-4 flex items-center w-full max-w-xs p-4 text-white bg-red-100 rounded-lg shadow dark:text-white dark:bg-red-800" role="alert">
-          <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-200 rounded-lg dark:bg-red-900 dark:text-red-200">
-            <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-              <path stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 2a8 8 0 1 1-8 8 8 8 0 0 1 8-8zm0 4v4m0 4h.01" />
-            </svg>
-            <span className="sr-only">Alert icon</span>
-          </div>
-          <div className="ms-3 text-sm font-normal">Please enter both your name and email.</div>
-          <button type="button" className="ms-auto -mx-1.5 -my-1.5 bg-red-100 text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-red-200 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-red-800 dark:hover:bg-red-700" onClick={() => setShowNotification(false)} aria-label="Close">
-            <span className="sr-only">Close</span>
-            <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-              <path stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-            </svg>
-          </button>
-        </div>
-      )}
     </>
   );
 };
